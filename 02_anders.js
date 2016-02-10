78b836e30ba59012003f152bcbf9b604dabdae3e
andersgibbons <andersgibbons@seattleacademy.org>
2016-02-09 16:08:26 -0800

    add 02_anders.js

--- 02_anders.js
+++ 02_anders.js
@@ -1,4 +1,15 @@
-console.log(process.argv);
 var pin = process.argv[2];
-var status = process.argv[3];
-console.log("pin number " + pin + " turn " + status);
+console.log(pin);
+var wpi = require('wiring-pi');
+var pin = 18;
+//console.log("wpi",wpi);
+wpi.setup('gpio');
+wpi.pinMode(pin*1,wpi.OUTPUT);
+var value = 1;
+function toggleBulb(){
+	wpi.digitalWrite(pin*1,value);
+	console.log("pin",pin,"value",value)
+	value = +!value;
+}
+
+setInterval(toggleBulb,500);
